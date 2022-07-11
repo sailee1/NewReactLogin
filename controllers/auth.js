cfe31c276f52492a5cfe86ead8e5113763e5bfde
sailee1 <saiclee1986@googlemail.com>
2022-07-11 20:00:24 +0100

    custom error handler and tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
-const { restart } = require('nodemon')
 const User = require('../models/User')
+const ErrorResponse = require("../utils/errorResponse")
 
 exports.register = async(req, res, next) => {
    const {username, email, password} = req.body
@@ -11,44 +11,34 @@ exports.register = async(req, res, next) => {
             password,
         })
 
-        res.status(201).json({
-            success: true, 
-            user, 
-        })
+       sendToken(user, 201, res)
 
     }catch(error){
-        res.status(500).json({
-            success: false, 
-            error: error.message, 
-        })
+       next(error)
     }
-
-
 }
+
 exports.login = async(req, res, next) => {
     const {email, password} = req.body 
 
     if(!email || !password){
-        res.status(400).json({success: false, error: "Please provide email and password"})
+        return next(new ErrorResponse("Please provide an email and password", 400))
     }
 
     try{
         const user = await User.findOne({email}).select("+password")
 
         if(!user){
-            res.status(404).json({success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Invalid credentials", 401))
         }
 
         const isMatch = await user.matchPasswords(password)
 
         if(!isMatch){
-            res.status(404).json({success: false, error: "Invalid credentials"})
+            return next(new ErrorResponse("Invalid credentials", 401))
         }
 
-        res.status(200).json({
-            success: true, 
-            token: "nonsense"
-        })
+        sendToken(user, 200, res)
         
 
     } catch(error){
@@ -63,4 +53,10 @@ exports.forgotpassword = (req, res, next) => {
 exports.resetpassword = (req, res, next) => {
     res.send("Reset Password Route")
 
+}
+
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken()
+    res.status(statusCode).json({success: true, token})
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: "./config.env"})
 const express = require("express")
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 
 connectDB()
 
@@ -11,7 +12,7 @@ app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
 
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
